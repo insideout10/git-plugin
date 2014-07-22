d23801c3e61c1f8ba092cdac2e6ea4a827882c27
David Riccitelli <david@insideout.io>
2014-07-22 19:45:45 +0300

    initial commit

---
+++ src/git.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: git
+ * Plugin URI: http://insideout.io
+ * Description: Automatically deploy plugins using Git
+ * Version: 1.0.0-SNAPSHOT
+ * Author: InsideOut10
+ * Author URI: http://insideout.io
+ * License: GPL
+ */
+
